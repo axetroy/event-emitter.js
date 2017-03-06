58281de495032ccc566a95e42d15648417419f1e
axetroy <troy450409405@gmail.com>
2017-03-07 01:57:22 +0800

    feat: handler apply arguments

--- index.js
+++ index.js
@@ -3,15 +3,15 @@
  */
 
 function randomId() {
-  let str = '', len = 20;
-  while (len--) {
-    str += String.fromCharCode(48 + ~~(Math.random() * 42));
-  }
-  return str + ':::' + new Date().getTime();
+  return Math.random().toString(36).substr(2, 16);
 }
 
 function EventEmitter() {
   this.e = {};
+}
+
+function findIndexById(id) {
+  return this.findIndex(callback => callback.__id__ === id);
 }
 
 const prototype = EventEmitter.prototype;
@@ -19,31 +19,24 @@ const prototype = EventEmitter.prototype;
 prototype.constructor = EventEmitter;
 
 prototype.on = function (event, handler) {
-  const container = this.e[event] = this.e[event] || [];
-  const id = randomId();
+  let container = this.e[event] = this.e[event] || [], id = randomId(), index;
   handler.__id__ = id;
   container.push(handler);
   return () => {
-    const index = container.findIndex(callback => callback.__id__ === id);
+    index = findIndexById.call(container, id);
     index >= 0 ? container.splice(index, 1) : void 0;
   }
 };
 
-prototype.off = function (event) {
-  this.e[event] = [];
-};
+prototype.off = event => this.e[event] = [];
 
-prototype.clear = function () {
-  this.e = {};
-};
+prototype.clear = this.e = {};
 
 prototype.once = function (event, handler) {
-  const container = this.e[event] = this.e[event] || [];
-  const _this = this;
-  const id = randomId();
+  let container = this.e[event] = this.e[event] || [], _this = this, id = randomId(), index;
 
   const callback = function () {
-    const index = container.findIndex(_callback => callback.__id__ === id);
+    index = findIndexById.call(container, id);
     index >= 0 ? container.splice(index, 1) : void 0;
     handler.apply(_this, arguments);
   };
@@ -53,10 +46,9 @@ prototype.once = function (event, handler) {
   container.push(callback);
 };
 
-prototype.emit = function (event, data) {
-  (this.e[event] || []).forEach((handler) => {
-    handler.call(this, data);
-  });
+prototype.emit = function () {
+  const argv = [].slice.call(arguments), event = argv.shift();
+  (this.e[event] || []).forEach((handler) => handler.apply(this, argv));
 };
 
 module.exports = EventEmitter;
